12d8888ea9ddb6c21205ab4bdc2067113aaf7e1a
Aaron Ma <aaronhma@gmail.com>
2019-07-30 21:31:56 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { DrawableDirective } from './drawable.directive';
+
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tfjs';
+export class AppComponent implements OnInit {
+  public linearModel: tf.Sequential;
+  public model: tf.LayersModel;
+  public predictions: Array<number> | undefined;
+  predictedNumber: string;
+
+  @ViewChild(DrawableDirective, {static: false}) canvas;
+
+  ngOnInit() {
+    this.loadModel();
+  }
+
+
+  //// LOAD PRETRAINED KERAS MODEL ////
+
+  async loadModel() {
+    this.model = await tf.loadLayersModel('assets/model.json');
+  }
+
+  async predict(imageData: ImageData | null) {
+    if (imageData === null) {
+      this.predictions = undefined;
+      return;
+    }
+
+    const pred = await tf.tidy(() => {
+
+      // Convert the canvas pixels to
+      let img = tf.browser.fromPixels(imageData, 1);
+      img = img.reshape([1, 28, 28, 1]);
+      img = tf.cast(img, 'float32');
+
+      // Make and format the predications
+      const output = this.model.predict(img) as any;
+
+      // Save predictions on the component
+      this.predictions = Array.from(output.dataSync());
+
+      for (let i = 0; i < this.predictions.length; i++) {
+        if (this.predictions[i] === 1) {
+          this.predictedNumber = i.toString();
+          console.log('Predicted number: ', this.predictedNumber)
+        }
+      }
+      if (this.predictedNumber === '') {
+        this.predictedNumber = ':(';
+        console.log('Predicted number: ', this.predictedNumber)
+      }
+    });
+
+  }
+
+  clear() {
+    this.predictedNumber = "";
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
+import { DrawableDirective } from './drawable.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrawableDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
